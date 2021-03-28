bc2b9959634ce1d3f334748e1ba8bd96d5e82e0f
krjovan <jovankron@gmail.com>
2021-03-28 22:40:21 +0200

    Admin add, update, delete user with modal dialog, change user role and reset password

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TokenPayload } from './authentication.service';
 import { User } from '../models/user';
 
 @Injectable({
@@ -15,29 +16,20 @@ export class UserService {
   public getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.API_URL + '/all');
   }
-/*
-  public getDeloviByKataloski(searchKataloski: string): Observable<Deo[]> {
-    let params = new HttpParams();
-    params = params.append('kataloskiBroj', searchKataloski);
-    this.httpClient.get<Deo[]>(this.API_URL, {params}).subscribe(data => {
-        this.dataChange.next(data);
-    },
-    (error: HttpErrorResponse) => {
-        console.log(error.name + ' ' + error.message);
-    });
 
-    return this.dataChange.asObservable();
+  public addUser(user: TokenPayload): Observable<any> {
+    return this.httpClient.post(this.API_URL + '/add', user);
   }
 
-  public addDeo(deo: Deo): Observable<any> {
-    return this.httpClient.post(this.API_URL, deo, httpOptions);
+  public updateUser(user): Observable<any> {
+    return this.httpClient.put(this.API_URL + '/admin/update/' + user._id, user);
   }
 
-  public updateDeo(deo: Deo): Observable<any> {
-    return this.httpClient.put(this.API_URL + '/' + deo.DEO_ID, deo, httpOptions);
+  public deleteUser(id): Observable<any> {
+    return this.httpClient.delete(this.API_URL + '/delete/' + id);
   }
 
-  public deleteDeo(id: number): Observable<any> {
-    return this.httpClient.delete(this.API_URL + '/' + id);
-  }*/
+  public resetPassword(user): Observable<any> {
+    return this.httpClient.put(this.API_URL + '/resetPassword', user);
+  }
 }
